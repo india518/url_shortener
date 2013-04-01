1a4981bef69759712afed6983873f4205fdb9c99
India and Nick <student@appacademy.io>
2013-04-01 16:59:10 -0700

    Adds path from long_urls to tag_types

--- app/models/tag_type.rb
+++ app/models/tag_type.rb
@@ -1,5 +1,7 @@
 class TagType < ActiveRecord::Base
   has_many :tags
+  has_many :short_urls, :through => :tags
+  has_many :long_urls, :through => :short_urls
 
   attr_accessible :type_of_tag
 end
