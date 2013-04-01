5f0821a539c91afa581806576ae539cc689be0e2
App Academy Student <student@appacademy.io>
2013-04-01 10:47:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+URLShortener::Application.config.secret_token = '3b59e563d29fd959a018c2e3c6fb43dcab24135b5431c886d8fe28a221886a4b19f718732132cfd0ff87106720dc52df4d8853f4a13266642378f6e75de38154'
